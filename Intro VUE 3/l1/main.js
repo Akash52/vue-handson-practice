01c9f9af85d228a3e7a3719427df1a8de7e96ae1
Akash52 <ac8572611@gmail.com>
2022-04-07 06:10:24 +0530

    Creating the Vue App

---
+++ Intro VUE 3/l1/main.js
@@ -0,0 +1,12 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      message: 'Hello Vue!',
+      name: 'Akash',
+      age: '21',
+      email: 'ac8572611@gmail.com',
+      password: 'Akash@321',
+      password2: 'Akash@321',
+    }
+  },
+})
